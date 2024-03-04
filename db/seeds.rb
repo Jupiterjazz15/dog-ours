bb138b866b12845172be9d69ba11793bdc1e8a95
coraliehaller <haller.coralie.15@gmail.com>
2024-03-04 17:12:03 +0100

    Seed pour les tags avec création d'un User et d'un Usertag

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,14 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Tag.destroy_all
+
+contents = %w[sport movies literature series gardening cooking]
+
+contents.each do |content|
+  Tag.create!(content: content)
+end
+
+user1 = User.create!(email: "test@gmail.com", password:"123456")
+UserTag.create!(user: user1 , tag: Tag.take)
