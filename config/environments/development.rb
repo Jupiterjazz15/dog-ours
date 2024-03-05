c57840576e2c3eafe3b6a565ec01c276079fb527
Antoine Lebeau <antoine.lebeau@gmail.com>
2024-03-05 17:41:56 +0100

    PWA, kinda fonctionnal due to Pundit

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,8 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = false
+
+  # config.hosts.clear
+  config.hosts << "534f-213-41-253-129.ngrok-free.app"
+  config.hosts << "534f-213-41-253-129.ngrok-free.app"
 end
