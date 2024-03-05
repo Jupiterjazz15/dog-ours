22290975bc3c20c84ff4957d9832d0c2fb6ddc94
antoineclavel <clavel.antoine@gmail.com>
2024-03-05 10:47:48 +0100

    mise en place des controller

--- app/controllers/user_tags_controller.rb
+++ app/controllers/user_tags_controller.rb
@@ -1,7 +1,14 @@
 class UserTagsController < ApplicationController
+  
   def create
+    @user_tag = UserTag.new(user_tag_params)
+    @user_tag.save
+    redirect_to user_path(@user_tag.user)
   end
 
   def destroy
+    @user_tag = UserTag.find(params[:id])
+    @user_tag.destroy
+    redirect_to user_path(@user_tag.user)
   end
 end
