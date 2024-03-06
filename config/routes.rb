1c0404d49ccf7d7d0a7d12b15f0fb9035c571472
coraliehaller <haller.coralie.15@gmail.com>
2024-03-06 12:12:18 +0100

    en cours pour le dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
   get "/dashboard", to: "pages#dashboard"
   get "/myprofile", to: "pages#myprofile"
+  get "/mywalks", to: "pages#mywalks"
 
   resources :dogs
 
