660475f0d9cae82e09a9b220c06e88e45e4e0d21
antoineclavel <clavel.antoine@gmail.com>
2024-03-06 12:09:04 +0100

    geocoder delivery brut

--- app/models/walk.rb
+++ app/models/walk.rb
@@ -6,7 +6,9 @@ class Walk < ApplicationRecord
   validates :difficulty, inclusion: { in: DIFFICULTIES }
   # Changement de :frequency en string (plus en boolean) + possibillité d'avoir nil si on ne veut pas mettre de récurrence
   validates :frequency, inclusion: { in: FREQUENCIES }, allow_nil: true
-
+  # GeoCoder
+  geocoded_by :starting_point
+  after_validation :geocode, if: :will_save_change_to_starting_point?
   # Methode pour pour gérer une walk réucrrente ou non cad frequency = nil
   def recurring?
     frequency != nil
