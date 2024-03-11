eb1ced98a93d24b8bd4b491d4634bce9c80568ff
coraliehaller <haller.coralie.15@gmail.com>
2024-03-11 15:05:29 +0100

    msg

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_07_113621) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_11_120400) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -123,7 +123,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_07_113621) do
 
   create_table "walks", force: :cascade do |t|
     t.string "starting_point"
-    t.integer "duration"
+    t.string "duration"
     t.datetime "start_time"
     t.string "difficulty"
     t.string "description"
