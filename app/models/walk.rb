7e2220852ffb925aed14585d9f4a7044b0b5510d
coraliehaller <haller.coralie.15@gmail.com>
2024-03-11 13:11:01 +0100

    Form new walk : presque fini

--- app/models/walk.rb
+++ app/models/walk.rb
@@ -1,6 +1,8 @@
 class Walk < ApplicationRecord
-  DIFFICULTIES = %w[Low Medium Intense]
+  DURATIONS = ["15 min", "30 min", "45 min", "1 h", "More than 1 h"]
+  DIFFICULTIES = ["Low", "Medium", "Intense"]
   FREQUENCIES = ["Once", "Every day", "Every other day"]
+  PARTICIPANTS = [1, 2, 3]
 
   validates :number_of_participant, numericality: { less_than_or_equal_to: 3 }
   belongs_to :user
@@ -9,8 +11,10 @@ class Walk < ApplicationRecord
   has_many :participants, through: :bookings, source: :user
   has_many :messages, dependent: :destroy
   has_many :walks, foreign_key: :parent_id, class_name: "Walk", dependent: :destroy
+  validates :duration, inclusion: { in: DURATIONS }
   validates :difficulty, inclusion: { in: DIFFICULTIES }
   validates :frequency, inclusion: { in: FREQUENCIES }, allow_nil: true
+  validates :number_of_participant, inclusion: { in: PARTICIPANTS }
   # GeoCoder
   geocoded_by :starting_point
   after_validation :geocode, if: :will_save_change_to_starting_point?
