e411c4298e1688c8cbbc95f9772bafd47fa1139b
Antoine Lebeau <antoine.lebeau@gmail.com>
2024-03-12 17:29:54 +0100

    coolman

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -16,7 +16,7 @@ class BookingsController < ApplicationController
     @booking.status = "pending"
     authorize @booking
     if @booking.save
-      redirect_to mywalks_path(created_booking: true)
+      redirect_to dashboard_path(created_booking: true)
     else
       render :new, status: :unprocessable_entity
     end
@@ -38,7 +38,7 @@ class BookingsController < ApplicationController
     @booking = Booking.find(params[:id])
     authorize @booking
     @booking.destroy
-    redirect_to booking_path(@booking)
+    redirect_to dashboard_path
   end
 
   private
