fc2ba70c4342f6181e9f661dd2323df6592d340d
coraliehaller <haller.coralie.15@gmail.com>
2024-03-12 12:22:37 +0100

    Ajout de l'autocomplete > il reste à améliorer le design de la searchbar

---
+++ app/javascript/controllers/address_autocomplete_controller.js
@@ -0,0 +1,20 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="address-autocomplete"
+export default class extends Controller {
+  static values = { apiKey: String }
+
+  static targets = ["address"]
+
+  connect() {
+    this.geocoder = new MapboxGeocoder({
+      accessToken: this.apiKeyValue,
+      types: "country,region,place,postcode,locality,neighborhood,address"
+    })
+    this.geocoder.addTo(this.element)
+  }
+
+  disconnect() {
+    this.geocoder.onRemove()
+  }
+}
