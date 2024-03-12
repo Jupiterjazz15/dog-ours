06c00158072dd6c23d02707eefa9d4db43f4ef39
coraliehaller <haller.coralie.15@gmail.com>
2024-03-12 09:16:26 +0100

    msg

--- db/schema.rb
+++ db/schema.rb
@@ -134,8 +134,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_11_120400) do
     t.float "latitude"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "name"
     t.bigint "parent_id"
+    t.string "name"
     t.index ["parent_id"], name: "index_walks_on_parent_id"
     t.index ["user_id"], name: "index_walks_on_user_id"
   end
