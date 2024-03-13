eef6721a32a540b546ef30d5745e9172b11fd67f
antoineclavel <clavel.antoine@gmail.com>
2024-03-13 11:57:12 +0100

    version intermediaire messagerie

--- app/javascript/controllers/walk_subscription_controller.js
+++ app/javascript/controllers/walk_subscription_controller.js
@@ -14,12 +14,28 @@ export default class extends Controller {
     )
   }
   #insertMessage(data) {
-    // event.preventDefault();
-    console.log("hello", data);
-    this.messagesTarget.insertAdjacentHTML('beforeend', data);
-    // const currentUserIsSender = this.currentUserIdValue === data.sender_id; 
+    // Logic to know if the sender is the current_user
+    const currentUserIsSender = this.currentUserIdValue === data.sender_id;
+    // Creating the whole message from the `data.message` String
+    const messageElement = this.#buildMessageElement(currentUserIsSender, data.message);
+    this.messagesTarget.insertAdjacentHTML('beforeend', messageElement);
     this.messagesTarget.scrollTo(0, this.messagesTarget.scrollHeight)
 
+  }
+  #buildMessageElement(currentUserIsSender, message) {
+    return `
+      <div class="message-row d-flex ${this.#justifyClass(currentUserIsSender)}">
+        <div class="${this.#userStyleClass(currentUserIsSender)}">
+          ${message}
+        </div>
+      </div>
+    `
+  }
+  #justifyClass(currentUserIsSender) {
+    return currentUserIsSender ? "justify-content-end" : "justify-content-start"
+  }
+  #userStyleClass(currentUserIsSender) {
+    return currentUserIsSender ? "user-message" : "message-member"
   }
   resetForm(event) {
     event.target.reset()
