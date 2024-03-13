1ba02858f9eb72bb12d23b0b6b5e1bfe27386bc4
coraliehaller <haller.coralie.15@gmail.com>
2024-03-13 12:44:57 +0100

    Fin de la seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -30,9 +30,9 @@ user = User.create!(first_name: "Antoine", last_name: "Lebeau", phone_number: "1
 file = URI.open("https://ca.slack-edge.com/T02NE0241-U06DG7195EE-41fa3d1f8d29-512")
 user.photo.attach(io: file, filename: "user.jpg", content_type: "image/jpeg")
 
-dog = Dog.create!(name: "Charlie", description: "The funniest dog in the world", birth_date: Date.new(2020, 8, 15), constraint: "Doesn't like small dogs", fun_fact: "The best swimmer you have ever seen !!",size: "Large", breed: Breed.find_by_content!("COLLIE ROUGH"), user_id: User.first.id)
+dog1 = Dog.create!(name: "Charlie", description: "The funniest dog in the world", birth_date: Date.new(2020, 8, 15), constraint: "Doesn't like small dogs", fun_fact: "The best swimmer you have ever seen !!",size: "Large", breed: Breed.find_by_content!("COLLIE ROUGH"), user_id: User.first.id)
 file = URI.open("https://img.passeportsante.net/1200x675/2021-05-06/i106625-signes-bonne-sante-chien.webp")
-dog.photo.attach(io: file, filename: "dog.webp", content_type: "image/webp")
+dog1.photo.attach(io: file, filename: "dog.webp", content_type: "image/webp")
 
 Walk.create!(name: "Walk in 75019", starting_point: "41 Av. Mathurin Moreau, 75019, Paris, France", duration: "15 min", start_time: DateTime.new(2024,03,25,12,30), difficulty: "Medium", description: "A walk in les Buttes-Chaumont" , frequency: "Once", number_of_participant: "1", user_id: User.first.id)
 
@@ -44,9 +44,9 @@ user = User.create!(first_name: "Coralie", last_name: "Haller", phone_number: "2
 file = URI.open("https://ca.slack-edge.com/T02NE0241-U06DG28K0CT-fafee26638ca-512")
 user.photo.attach(io: file, filename: "user.jpg", content_type: "image/jpeg")
 
-dog = Dog.create!(name: "Molly", description: "Lovely, cute and very friendly", birth_date: Date.new(2022, 10, 10), constraint: "None", fun_fact: "She loves to wear costume",size: "Small", breed: Breed.find_by_content!("PUG"), user_id: User.second.id)
+dog2 = Dog.create!(name: "Molly", description: "Lovely, cute and very friendly", birth_date: Date.new(2022, 10, 10), constraint: "None", fun_fact: "She loves to wear costume",size: "Small", breed: Breed.find_by_content!("PUG"), user_id: User.second.id)
 file =  URI.open("https://cdn.britannica.com/35/233235-050-8DED07E3/Pug-dog.jpg")
-dog.photo.attach(io: file, filename: "dog.webp", content_type: "image/webp")
+dog2.photo.attach(io: file, filename: "dog.webp", content_type: "image/webp")
 
 Walk.create!(name: "Walk in 75007", starting_point: "24 Rue de Varenne, 75007, Paris, France", duration: "30 min", start_time: DateTime.new(2024,03,27,18,00), difficulty: "Medium", description: "A walk that I like since we can walk in large streets and we can stop in a park." , frequency: "Once",number_of_participant: "2", user_id: User.second.id)
 
@@ -62,9 +62,9 @@ user = User.create!(first_name: "Guillaume", last_name: "Dupuis", phone_number: 
 file = URI.open("https://ca.slack-edge.com/T02NE0241-U06D6P7V0UX-2fe19bee1d04-512")
 user.photo.attach(io: file, filename: "user.jpg", content_type: "image/jpeg")
 
-dog = Dog.create!(name: "Sam", description: "The funniest dog in the world", birth_date: Date.new(2020, 8, 15), constraint: "No constraint", fun_fact: "Loves cats", size: "Large", breed: Breed.find_by_content!("AUSTRALIAN SHEPHERD"), user_id: User.third.id)
+dog3 = Dog.create!(name: "Sam", description: "The funniest dog in the world", birth_date: Date.new(2020, 8, 15), constraint: "No constraint", fun_fact: "Loves cats", size: "Large", breed: Breed.find_by_content!("AUSTRALIAN SHEPHERD"), user_id: User.third.id)
 file = URI.open("https://img.20mn.fr/2c2xoZqdQhu84Dmhb8ci9Sk/1444x920_media")
-dog.photo.attach(io: file, filename: "dog.webp", content_type: "image/webp")
+dog3.photo.attach(io: file, filename: "dog.webp", content_type: "image/webp")
 
 Walk.create!(name: "Walk in 75011", starting_point: "140 rue oberkampf, 75011, Paris, France", duration: "30 min", start_time: DateTime.new(2024,04,01,12,00), difficulty: "Medium", description: "My favourite walk!", frequency: "Every other day",number_of_participant: "2", user_id: User.third.id)
 
@@ -76,9 +76,9 @@ user = User.create!(first_name: "Antoine", last_name: "Clavel", phone_number: "4
 file = URI.open("https://ca.slack-edge.com/T02NE0241-U06EKJ558NL-g43dd42946ee-512")
 user.photo.attach(io: file, filename: "user.jpg", content_type: "image/jpeg")
 
-dog = Dog.create!(name: "Jim", description: "The funniest dog in the world", birth_date: Date.new(2018, 10, 10), constraint: "Doesn't like small dogs", fun_fact: "The best swimmer you have ever seen !!", size: "Large", breed: Breed.find_by_content!("AMERICAN STAFFORDSHIRE TERRIER"), user_id: User.fourth.id)
+dog4 = Dog.create!(name: "Jim", description: "The funniest dog in the world", birth_date: Date.new(2018, 10, 10), constraint: "Doesn't like small dogs", fun_fact: "The best swimmer you have ever seen !!", size: "Large", breed: Breed.find_by_content!("AMERICAN STAFFORDSHIRE TERRIER"), user_id: User.fourth.id)
 file = URI.open("https://www.rustica.fr/images/risques-si-je-possede-chien-dont-race-est-interdite-france-19189-l790-h526.jpg.webp")
-dog.photo.attach(io: file, filename: "dog.webp", content_type: "image/webp")
+dog4.photo.attach(io: file, filename: "dog.webp", content_type: "image/webp")
 
 walk = Walk.create!(name: "My favorite walk", starting_point: "45 rue Saint Antoine, 75004, Paris, France", duration: "30 min", start_time: DateTime.new(2024,03,25,12,30), difficulty: "Medium", description: "A cool walk! ", frequency: "Every other day", number_of_participant: "1", user_id: User.fourth.id)
 
@@ -90,7 +90,7 @@ end
 
 Walk.create!(name: "Walk in 75004", starting_point: "45 rue Saint Antoine, 75004, Paris, France", duration: "30 min", start_time: DateTime.new(2024,03,18,13,00), difficulty: "Medium", description: "A peaceful walk." , frequency: "Once",number_of_participant: "2", user_id: User.fourth.id)
 
-Booking.create!(status: "Accepted", user_id: User.fourth.id, walk_id: Walk.where(user_id: User.fourth.id).first&.id)
+Booking.create!(status: "Accepted", user_id: User.fourth.id, walk_id: Walk.where( .first&.id)
 
 p "#{User.fourth.first_name} has #{Walk.where(user_id: User.fourth.id).count} walk(s) and #{Booking.where(user_id: User.fourth.id).count} booking(s)"
 
@@ -100,9 +100,9 @@ user = User.create!(first_name: "Clothilde", last_name: "Scache", phone_number: 
 file = URI.open("https://ca.slack-edge.com/T02NE0241-U06EHGW24V6-4177402b7800-512")
 user.photo.attach(io: file, filename: "user.jpg", content_type: "image/jpeg")
 
-dog = Dog.create!(name: "Mac", description: "A kind and joyful dog!", birth_date: Date.new(2019, 05, 10), constraint: "He has no constraint", fun_fact: "He helps me to be motivated to run!", size: "Large", breed: Breed.find_by_content!("GERMAN SHORT- HAIRED POINTING DOG"), user_id: User.fifth.id)
+dog5 = Dog.create!(name: "Mac", description: "A kind and joyful dog!", birth_date: Date.new(2019, 05, 10), constraint: "He has no constraint", fun_fact: "He helps me to be motivated to run!", size: "Large", breed: Breed.find_by_content!("GERMAN SHORT- HAIRED POINTING DOG"), user_id: User.fifth.id)
 file = URI.open("https://www.animaleco.com/wp-content/uploads/2023/03/175023298_m.jpg")
-dog.photo.attach(io: file, filename: "dog.jpg", content_type: "image/jpeg")
+dog5.photo.attach(io: file, filename: "dog.jpg", content_type: "image/jpeg")
 
 Walk.create!(name: "Walk in 75017", starting_point: "175 Avenue de Wagram,75017, Paris, France", duration: "1 h", start_time: DateTime.new(2024,03,18,13,00), difficulty: "Medium", description: "A long walk to expend energy" , frequency: "Once",number_of_participant: "3", user_id: User.fifth.id)
 
@@ -114,9 +114,9 @@ user6 = User.create!(first_name: "Gabrielle ", last_name: "Simha", phone_number:
 file = URI.open("https://ca.slack-edge.com/T02NE0241-U06E791DEU9-b543cb2a1db7-512")
 user6.photo.attach(io: file, filename: "user.jpg", content_type: "image/jpeg")
 
-dog = Dog.create!(name: "Will", description: "The best dog!", birth_date: Date.new(2017, 05, 10), constraint: "He doesn't like other male dogs", fun_fact: "He loves tree branches", size: "Large", breed: Breed.find_by_content!("KANGAL SHEPHERD DOG"), user_id: user6.id)
+dog6 = Dog.create!(name: "Will", description: "The best dog!", birth_date: Date.new(2017, 05, 10), constraint: "He doesn't like other male dogs", fun_fact: "He loves tree branches", size: "Large", breed: Breed.find_by_content!("KANGAL SHEPHERD DOG"), user_id: user6.id)
 file = URI.open("https://lemagduchien.ouest-france.fr/images/dossiers/2018-10/kangal-1-134823.jpg")
-dog.photo.attach(io: file, filename: "dog.jpg", content_type: "image/jpeg")
+dog6.photo.attach(io: file, filename: "dog.jpg", content_type: "image/jpeg")
 
 Walk.create!(name: "Walk in 75020", starting_point: "16 Rue Ramponeau,75020, Paris, France", duration: "30 min", start_time: DateTime.new(2024,03,20,17,30), difficulty: "Medium", description: "A long walk with special spot to play" , frequency: "Once",number_of_participant: "2", user_id: user6.id)
 
@@ -127,9 +127,9 @@ user7 = User.create!(first_name: "Pierre", last_name: "Urban", phone_number: "45
 file = URI.open("https://ca.slack-edge.com/T02NE0241-U06DZJW1W68-82c2aa6b1622-512")
 user7.photo.attach(io: file, filename: "user.jpg", content_type: "image/jpeg")
 
-dog = Dog.create!(name: "Bretzel", description: "She has so much energy!", birth_date: Date.new(2018, 07, 18), constraint: "None: she is very kind tolerant", fun_fact: "She loves cats!", size: "Medium", breed: Breed.find_by_content!("BEAGLE HARRIER"), user_id: user7.id)
+dog7 = Dog.create!(name: "Bretzel", description: "She has so much energy!", birth_date: Date.new(2018, 07, 18), constraint: "None: she is very kind tolerant", fun_fact: "She loves cats!", size: "Medium", breed: Breed.find_by_content!("BEAGLE HARRIER"), user_id: user7.id)
 file = URI.open("https://www.kingpet.fr/ph/u/766721643537408/6401019845823116/be3bff0c84db51867cdc5a58996262031.jpg?align=54,43&v=31&fm=webp&w=1080&h=810&q=35")
-dog.photo.attach(io: file, filename: "dog.webp", content_type: "image/webp")
+dog7.photo.attach(io: file, filename: "dog.webp", content_type: "image/webp")
 
 Walk.create!(name: "B. Vincennes ", starting_point: "26 Rue du Rendez-Vous, 75012, Paris, France", duration: "1 h", start_time: DateTime.new(2024,03,18,19,0), difficulty: "Medium", description: "A walk around the lac Daumesnil" , frequency: "Once",number_of_participant: "1", user_id: user7.id)
 
@@ -140,9 +140,9 @@ user8 = User.create!(first_name: "Antonin", last_name: "Danto", phone_number: "4
 file = URI.open("https://ca.slack-edge.com/T02NE0241-U06DX3RAEAG-4e517ec9aca6-512")
 user8.photo.attach(io: file, filename: "user.jpg", content_type: "image/jpeg")
 
-dog = Dog.create!(name: "Pirouette", description: "A very fun buddy!", birth_date: Date.new(2021, 01, 23), constraint: "No constraint", fun_fact: "She loves to swim!", size: "Small", breed: Breed.find_by_content!("MINIATURE PINSCHER"), user_id: user8.id)
+dog8 = Dog.create!(name: "Pirouette", description: "A very fun buddy!", birth_date: Date.new(2021, 01, 23), constraint: "No constraint", fun_fact: "She loves to swim!", size: "Small", breed: Breed.find_by_content!("MINIATURE PINSCHER"), user_id: user8.id)
 file = URI.open("https://www.zooplus.fr/magazine/wp-content/uploads/2023/03/pinscher-nain-sur-une-couverture.jpg")
-dog.photo.attach(io: file, filename: "dog.jpg", content_type: "image/jpeg")
+dog8.photo.attach(io: file, filename: "dog.jpg", content_type: "image/jpeg")
 
 Walk.create!(name: "J. Luxembourg", starting_point: "9 Rue Férou, 75006, Paris, France", duration: "15 min", start_time: DateTime.new(2024,03,22,18,0), difficulty: "Low", description: "A walk in the jardin du Luxembourg", frequency: "Once",number_of_participant: "2", user_id: user8.id)
 
@@ -153,9 +153,9 @@ user9 = User.create!(first_name: "Ahmed", last_name: "Boussaada", phone_number: 
 file = URI.open("https://ca.slack-edge.com/T02NE0241-U06DUHU6VT4-cf7e45775c6c-512")
 user9.photo.attach(io: file, filename: "user.jpg", content_type: "image/jpeg")
 
-dog = Dog.create!(name: "Kiwi", description: "He is very kind and he loves to meet new dogs!", birth_date: Date.new(2022, 04, 03), constraint: "He is affraid of labradors", fun_fact: "He snores very loudly!", size: "Small", breed: Breed.find_by_content!("CAIRN TERRIER"), user_id: user9.id)
+dog9 = Dog.create!(name: "Kiwi", description: "He is very kind and he loves to meet new dogs!", birth_date: Date.new(2022, 04, 03), constraint: "He is affraid of labradors", fun_fact: "He snores very loudly!", size: "Small", breed: Breed.find_by_content!("CAIRN TERRIER"), user_id: user9.id)
 file = URI.open("https://www.yorkshires.fr/wp-content/uploads/2023/09/cairn-terrier.jpg")
-dog.photo.attach(io: file, filename: "dog.jpg", content_type: "image/jpeg")
+dog9.photo.attach(io: file, filename: "dog.jpg", content_type: "image/jpeg")
 
 Walk.create!(name: "Sqr Pablo-C", starting_point: "59 Rue de Lourmel, 75015, Paris, France", duration: "30 min", start_time: DateTime.new(2024,03,28,20,0), difficulty: "Low", description: "A walk in the Square Pablo-Casals" , frequency: "Once",number_of_participant: "1", user_id: user9.id)
 
@@ -189,5 +189,5 @@ p "You have created #{Tag.count} tags"
 UserTag.create!(user: User.first, tag: Tag.take)
 UserTag.create!(user: User.first, tag: Tag.take)
 
-file = URI.open("https://www.rustica.fr/images/risques-si-je-possede-chien-dont-race-est-interdite-france-19189-l790-h526.jpg.webp")
-dog.photo.attach(io: file, filename: "dog.webp", content_type: "image/webp")
+#file = URI.open("https://www.rustica.fr/images/risques-si-je-possede-chien-dont-race-est-interdite-france-19189-l790-h526.jpg.webp")
+#dog.photo.attach(io: file, filename: "dog.webp", content_type: "image/webp")
